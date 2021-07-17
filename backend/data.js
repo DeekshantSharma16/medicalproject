b0c9e5a0e0adeb41ec7bbc9db6927539ed550d71
Deekshant <deekshant@ubitechsolutions.com>
2021-07-17 15:03:48 +0530

    done

--- backend/data.js
+++ backend/data.js
@@ -4,7 +4,7 @@ const  data = {
                 {
                         name:'Deekshant',
                         email:'admin@example.com',
-                        password: bcrypt.hashSync('123456',8),
+                        password: bcrypt.hashSync('1234',8),
                         isAdmin: true,
                 },
                 {
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,21 +1,23 @@
-import express from "express";
-import expressAsyncHandler from "express-async-handler";
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
-import data from "../data.js";
-import User from "../models/userModel.js";
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
 userRouter.get(
-  "/seed",
+  '/seed',
   expressAsyncHandler(async (req, res) => {
     // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
   })
 );
+
 userRouter.post(
-  "/signin",
+  '/signin',
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -30,7 +32,7 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({ message:"Invalid Email or password"});
+    res.status(401).send({ message: 'Invalid email or password' });
   })
 );
 
