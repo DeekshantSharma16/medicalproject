0d03e1141883c89b2dd942eee35e1b097b4b2bea
Deekshant <deekshant@ubitechsolutions.com>
2021-07-17 18:41:38 +0530

    signup page done

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,9 +1,12 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
-import Axios from "axios";
+import Axios from 'axios';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants';
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
-
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
@@ -15,10 +18,15 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       qty,
     },
   });
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
 
 export const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: productId });
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
